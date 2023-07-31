d4d5477c66ddf02f535b80ac1a47454a40d3a57f
Ashrayergso <133031666+Ashrayergso@users.noreply.github.com>
2023-07-30 23:57:32 -0400

    Revert "Build a Django application to automate crew member assignment to ships."

--- crew_assignment_project/crew_assignment_project/urls.py
+++
@@ -1,9 +0,0 @@
-```python
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('crew_assignment_app.urls')),
-]
-```
